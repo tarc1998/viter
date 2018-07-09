a3cde4cffcdeeccb1d08d869d6839c2a4ea9ac97
kuba <kuba.grudzien1999.jg@gmail.com>
2018-07-09 14:17:38 +0200

    test

--- viter.py
+++ viter.py
@@ -3,14 +3,15 @@ import numpy as np
 
 n=5
 m=5
+iter = n+m+10
 goal=(0,0)
 gamma=1
 
 dir_r=[-1,0,1,0]
 dir_c=[0,1,0,-1]
 print ( "nic")
-val1=np.matrix([[0]*m]*n)
-val2=np.random.rand(n, m)
+v1=np.matrix([[0]*m]*n)
+v2=np.random.rand(n, m)
 
 reward=np.array([[[-1]*4]*m]*n)
 prob=np.array([[[[[0]*m]*n]*m]*n]*4)
@@ -30,7 +31,17 @@ for i in range(0,4):
     prob[i][0][0][0][0] = 0
 			
 
-while(np.array_equal(val1, val2)):
+for u in range (0,iter):
   for i in range(0,n):
     for j in range(0,m):
-        
+        maks = -1000
+        for k1 in range (0,n):
+            for k2 in range (0,m):
+                for k_dir in range(0, 4):
+                if( reward[k_dir][k1][k2] + gamma * prob[k_dir][i][j][k1][k2] * v1[k1][k2] > maks )
+                    maks =  reward[k_dir][k1][k2] + gamma * prob[k_dir][i][j][k1][k2] * v1[k1][k2]
+        v2[i][j] = maks
+   for i in range (0,n):
+       for j in range(0, m):
+           v1[i][j] = v2[i][j]
+
