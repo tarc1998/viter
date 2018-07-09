417685a35d171d8a7c8660f2db5141a8a3ccf0b9
kuba <kuba.grudzien1999.jg@gmail.com>
2018-07-09 14:07:39 +0200

    test

--- viter.py
+++ viter.py
@@ -15,18 +15,22 @@ val2=np.random.rand(n, m)
 reward=np.array([[[-1]*4]*m]*n)
 prob=np.array([[[[[0]*m]*n]*m]*n]*4)
 
+
+//setting dependent probabilities
 for i in range(0,n):
   for j in range(0,m):
     for k in range(0,4):
       if 0 <= i+dir_r[k] < n && 0<=j+dir_c[k] <m:
-     	prob[4][][m][n][m] = 1
+     	  prob[4][i][j][i+dir_r[k]][j+dir_c[k]] = 1
       else:
-	pro[k][
-	
+          prob[4][i][j][i + dir_r[k]][j + dir_c[k]] = 0
+
+//the upper left corner keeps
+for i in range(0,4):
+    prob[i][0][0][0][0] = 0
 			
 
 while(np.array_equal(val1, val2)):
   for i in range(0,n):
     for j in range(0,m):
-      if((i,j)!=goal):
-	print('asud')
+        
