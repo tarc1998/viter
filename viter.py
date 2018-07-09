3faec2a41bb36a80a40060caae96c22c5e591abf
kuba <kuba.grudzien1999.jg@gmail.com>
2018-07-09 14:22:41 +0200

    Załączam kod to samouczącego się systemu poruszającym się po gridzie 4x4. pozdrawiam i rzyczę powodzenia zdrowie! kuba

--- viter.py
+++ viter.py
@@ -17,31 +17,34 @@ reward=np.array([[[-1]*4]*m]*n)
 prob=np.array([[[[[0]*m]*n]*m]*n]*4)
 
 
-//setting dependent probabilities
+#setting dependent probabilities
 for i in range(0,n):
   for j in range(0,m):
     for k in range(0,4):
-      if 0 <= i+dir_r[k] < n && 0<=j+dir_c[k] <m:
+      if 0 <= i+dir_r[k] < n and 0<=j+dir_c[k] <m:
      	  prob[4][i][j][i+dir_r[k]][j+dir_c[k]] = 1
       else:
           prob[4][i][j][i + dir_r[k]][j + dir_c[k]] = 0
 
-//the upper left corner keeps
+#the upper left corner keeps
 for i in range(0,4):
     prob[i][0][0][0][0] = 0
 			
 
-for u in range (0,iter):
+for u in range(0,iter):
   for i in range(0,n):
     for j in range(0,m):
         maks = -1000
-        for k1 in range (0,n):
-            for k2 in range (0,m):
+        for k1 in range(0,n):
+            for k2 in range(0,m):
                 for k_dir in range(0, 4):
-                if( reward[k_dir][k1][k2] + gamma * prob[k_dir][i][j][k1][k2] * v1[k1][k2] > maks )
-                    maks =  reward[k_dir][k1][k2] + gamma * prob[k_dir][i][j][k1][k2] * v1[k1][k2]
-        v2[i][j] = maks
+                    if reward[k_dir][k1][k2] + gamma * prob[k_dir][i][j][k1][k2] * v1[k1][k2] > maks :
+                        maks =  reward[k_dir][k1][k2] + gamma * prob[k_dir][i][j][k1][k2] * v1[k1][k2]
+    v2[i][j] = maks
    for i in range (0,n):
        for j in range(0, m):
            v1[i][j] = v2[i][j]
 
+for i in range( 0, n):
+    for j in range (0, m);
+    print ( v1)
